270d2c822485abbb0663e2e4bf1efb0b1699a8f7
PringlesHair <evil8676@naver.com>
2023-02-01 15:46:02 +0900

    distribution

---
+++ src/main/java/com/example/todo/config/CorsConfig.java
@@ -0,0 +1,22 @@
+package com.example.todo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+//         api cors 정책 설정
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:3000",
+                        "http://practice-s3-jck-bucket001.s3-website.ap-northeast-2.amazonaws.com") // api요청 허용 URL
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
